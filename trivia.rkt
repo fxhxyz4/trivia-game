92c959b389ce2c3ffa0fc847fb19d089eccc0259
fxhxyz <fxhxyz7@proton.me>
2025-06-28 09:24:38 +0000

    added correct/incorrect counters & added hint system

--- trivia.rkt
+++ trivia.rkt
@@ -2,30 +2,30 @@
 
 #|
   Author: fxhxyz
+  License: MIT license
   Email: fxhsec@proton.me
   Website: fxhxyz.vercel.app
 |#
 
-; Import modules
 (require racket/system)
 (require json)
 
-; The dsp function encapsulates the use of the display function.
-; It takes an argument x and displays it on the screen using display.
+(define hyphen "_")
+
+; Incapsulate display func
 (define (dsp x)
   (cond
     [(string? x) (display x)]
     [else "Error"]))
 
-; Function for displaying logo.
 (define (logo)
   (for-each dsp '(" \n"
-                  "  _        _       _       \n"
-                  " | |      (_)     (_)      \n"
-                  " | |_ _ __ ___   ___  __ _ \n"
-                  " | __| '__| \\ \\ / / |/ _` |\n"
-                  " | |_| |  | |\\ V /| | (_| |\n"
-                  "  \\__|_|  |_| \\_/ |_|\\__,_|\n"
+                  "  _        _       _       _       \n"
+                  " | |      (_)     (_)      (_)     \n"
+                  " | |_ _ __ ___   ___  __ _         \n"
+                  " | __| '__| \\ \\ / / |/ _` |        \n"
+                  " | |_| |  | |\\ V /| | (_| |        \n"
+                  "  \\__|_|  |_| \\_/ |_|\\__,_|        \n"
                   " \n"
                   " \n")))
 
@@ -35,28 +35,51 @@
   (values (map (λ (qa) (hash-ref qa 'question)) (hash-ref json-data 'questions))
           (map (λ (qa) (hash-ref qa 'answer)) (hash-ref json-data 'questions))))
 
-; Output question and process answer.
-; Using tail recursion.
+; Hint manager
+(define (make-hint word count)
+  (let* ([lst (string->list word)]
+         [len (length lst)]
+         [revealed (for/list ([i (in-range len)])
+                     (if (< i count)
+                         (string (list-ref lst i))
+                         hyphen))])
+    (string-join revealed "")))
+
+; Process a single question, return 1 if correct, number of incorrect attempts
 (define (process-q question answer)
-  (let process-q-tail ()
+  (let loop ([incorrect-count 0])
+    (define hint (make-hint answer incorrect-count))
     (dsp question)
-    (dsp "\nYour answer: ")
+    (dsp (string-append "\nHint: " hint "\n\n"))
+    (dsp "Your answer: ")
     (flush-output)
-    (if (string-ci=? (string-trim (read-line)) answer)
-        (begin
-          (dsp "\nCorrect answer\n\n\n"))
-        (begin
-          (dsp "\nIncorrect answer\n\n\n")
-          (process-q question answer)))))
+    (let ([user-answer (string-trim (read-line))])
+      (if (string-ci=? user-answer answer)
+          (begin
+            (dsp "\nCorrect answer\n\n\n")
+            (list 1 incorrect-count))
+          (begin
+            (dsp "\n\n\nIncorrect answer\n\n\n")
+            (loop (+ incorrect-count 1)))))))
 
-; Applies the process-q function to each question-answer pair.
+; Process all questions, accumulate total correct and incorrect answers
 (define (process-que questions answers)
-  (map process-q questions answers))
+  (define (helper qs as total-correct total-incorrect)
+    (if (or (null? qs) (null? as))
+        (values total-correct total-incorrect)
+        (let ([result (process-q (car qs) (car as))])
+          (helper (cdr qs) (cdr as)
+                  (+ total-correct (first result))
+                  (+ total-incorrect (second result))))))
+  (helper questions answers 0 0))
 
-; Loop through all questions and process answers.
+; Main entry point
 (define (main)
   (logo)
-  (let-values ([(questions answers) (read-q "./data.json")])
-    (process-que questions answers)))
+  (let-values ([(questions answers) (read-q "/home/fxhxyz/Desktop/dev/trivia-game/data.json")])
+    (let-values ([(total-correct total-incorrect) (process-que questions answers)])
+      (dsp (format "Game finished!\nTotal correct answers: ~a\nTotal incorrect attempts: ~a\n"
+                   total-correct total-incorrect)))))
 
+; Main call
 (main)
